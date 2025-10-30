9ce1a3cab4e664d9f732bdce30dec3b36b5af10e
atul-bagde-sasvatest <atul_bagde@persistent.com>
2025-10-30 17:42:45 +0530

    Update performanceissue.ts

--- performanceissue.ts
+++ performanceissue.ts
@@ -1,11 +1,7 @@
 
-function sumArray(arr: number[]): number {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr.reduce((a, b) => a + b, 0); // Inefficient: recalculates sum every time
-  }
-  return sum;
-}
+function printNumbers(arr: number[], num: number) {
+    for (let i = 0; i < 1000000; i++) {
 
-const numbers = [1, 2, 3, 4, 5];
-console.log(sumArray(numbers)); // Output: 75
+    }
+    console.log(num);
+}
